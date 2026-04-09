a9db438a8371cc5eb4dc67d231722494be95c706
onelx <garciaosvaldo@gmail.com>
2026-04-09 19:37:08 -0300

    feat: add next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,16 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**.supabase.co',
-        pathname: '/storage/v1/object/public/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
       },
     ],
   },
@@ -23,6 +19,6 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '5mb',
     },
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
