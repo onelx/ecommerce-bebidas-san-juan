85c77283499dc96901dcc9e46199bfefe57149fb
onelx <garciaosvaldo@gmail.com>
2026-04-09 19:25:39 -0300

    feat: add next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,32 +6,23 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: '**.supabase.co',
-        pathname: '/storage/v1/object/public/**'
+        pathname: '/storage/v1/object/public/**',
       },
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com'
-      }
-    ]
-  },
-  async headers() {
-    return [
+        hostname: 'images.unsplash.com',
+      },
       {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
-        ]
-      }
-    ]
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+    ],
   },
   experimental: {
     serverActions: {
-      bodySizeLimit: '2mb'
-    }
-  }
+      bodySizeLimit: '5mb',
+    },
+  },
 }
 
 export default nextConfig
