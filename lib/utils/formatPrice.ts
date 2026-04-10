7a970b85e4b102014d502fbae90fececa8f25695
Osvaldo <osvaldo@MacBook-Pro-de-Osvaldo.local>
2026-04-09 21:59:48 -0300

    fix: add missing lib/utils/formatPrice module

---
+++ lib/utils/formatPrice.ts
@@ -0,0 +1,8 @@
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: 'ARS',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price);
+}
